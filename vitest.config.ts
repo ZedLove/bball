a873d2e743bf2069659a733fe06ca63bb334b078
Zak Nitsch <zaknitsch@gmail.com>
2026-04-10 20:34:02 -0700

    test: configure coverage thresholds in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,13 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     coverage: {
-      reporter: ['text', 'html'],
+      reporter: ['text', 'html', 'json'],
+      thresholds: {
+        lines: 90,
+        functions: 100,
+        branches: 85,
+        statements: 90,
+      },
     },
     include: ["**/*.test.ts"]
   },
