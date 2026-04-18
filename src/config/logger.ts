59d0f601dec31e2300f2b3f6689f5014eb8740d0
Zak Nitsch <zaknitsch@gmail.com>
2026-04-18 00:01:22 -0700

    chore(config): suppress logger output in test mode

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,7 +1,12 @@
 import { createLogger, format, transports } from 'winston';
 
 export const logger = createLogger({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  level:
+    process.env.VITEST === 'true'
+      ? 'silent'
+      : process.env.NODE_ENV === 'production'
+        ? 'info'
+        : 'debug',
   format: format.combine(
     format.timestamp(),
     format.errors({ stack: true }),
