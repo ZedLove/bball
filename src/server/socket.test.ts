2a6d326fc15ff492a4cd36360620932b46a97652
Zak Nitsch <zaknitsch@gmail.com>
2026-04-27 08:20:22 -0700

    refactor(scheduler): update logger for unified live tracking mode

--- src/server/socket.test.ts
+++ src/server/socket.test.ts
@@ -57,7 +57,7 @@ const SAMPLE_UPDATE: GameUpdate = {
   delayDescription: null,
   isExtraInnings: false,
   scheduledInnings: 9,
-  trackingMode: 'outs',
+  trackingMode: 'live',
   outsRemaining: 2,
   totalOutsRemaining: 8,
   runsNeeded: null,
